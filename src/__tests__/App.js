722e5422164c8d250c2408358f68941957040c3b
Gabriel Fouasnon <gabriel@fouasnon.com>
2020-11-27 16:13:38 -0700

    Fix tests after sdk update

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 import App from '../App.tsx';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { act, create } from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  await act(async () => {
+    create(<App />);
+  });
 });
