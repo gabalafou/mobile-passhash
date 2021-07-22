f057787c8039b82f1c8e266d37329e1cd2683495
Gabriel Fouasnon <gabriel@fouasnon.com>
2021-07-22 14:38:09 -0500

    Fix warning from test runner

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -5,6 +5,8 @@ import App from '../App.tsx';
 // Note: test renderer must be required after react-native.
 import { act, create } from 'react-test-renderer';
 
+// https://stackoverflow.com/questions/59587799/how-to-resolve-animated-usenativedriver-is-not-supported-because-the-native
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
 it('renders correctly', async () => {
   await act(async () => {
